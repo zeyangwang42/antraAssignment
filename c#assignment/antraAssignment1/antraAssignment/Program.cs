2274d52acd0bc9eafb8e56c797e601966b003653
zeyangwang42 <65516494+zeyangwang42@users.noreply.github.com>
2021-11-17 17:08:45 -0600

    Add files via upload

---
+++ c#assignment/antraAssignment1/antraAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿public class CAssignment
+{
+    public int []  repeatArray(int [] input)
+    {
+        int [] result = new int [input.Length*2];
+        for(int i =0; i < input.Length; i++)
+        {
+           result [i] = input [i];
+            result[i + input.Length] = input[i];
+        }
+        
+        return result;
+    }
+
+    public static void Main()
+    {
+
+        int[] input = { 1, 2, 3 };
+
+        CAssignment test = new();
+        int [] output = test.repeatArray(input);
+
+
+      
+
+        foreach ( var nums in output)
+        {
+
+            Console.WriteLine(nums);
+        }
+
+    }
+
+
+}
