cd77d39a4242e342d72b53d99d0023d4a9f10665
zeyangwang42 <65516494+zeyangwang42@users.noreply.github.com>
2021-11-18 22:45:49 -0600

    Add files via upload

---
+++ c#assignment/Demo/Exercise2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Exercise2
+{
+    public class Arithmetic
+    {
+        int a,b;
+      public void Addition()
+        {
+            Console.WriteLine("Addition reulut: " + (a+b));
+        }
+        public void subtraction()
+        {
+            Console.WriteLine("Subtraction reulut: " + (a - b));
+        }
+        public void multiplication()
+        {
+            Console.WriteLine("Multiplication reulut: " + (a * b));
+        }
+        public void division()
+        {
+            Console.WriteLine(" Division reulut: " + (a / b));
+        }
+        public void GetData()
+        {
+            Console.WriteLine("Enter First value");
+            string input1 = Console.ReadLine();
+            Int32.TryParse(input1, out a);
+            Console.WriteLine("Enter Second value");
+            string input2 = Console.ReadLine();
+            Int32.TryParse(input2, out b);
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmetic a1 = new();
+            a1.GetData();
+            Console.WriteLine("Enter Arithmetic type:Addition, Subtraction, Multiplication or Division");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "Addition":
+                    a1.Addition();
+                    break;
+                case "Subtraction":
+                    a1.subtraction();
+                    break;
+                case "Multiplication":
+                    a1.multiplication();
+                    break;
+                case "Division":
+                    a1.division();
+                    break;
+                default:
+                    Console.WriteLine($"Arithmetic type need to be :Addition, Subtraction, Multiplication and Division");
+                    break;
+            }
+          
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#assignment/Demo/Exercise3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise3
+{
+    public class reverseString
+    {
+        string s1;
+        public void GetData()
+        {
+            Console.WriteLine("Enter your String");
+            s1 = Console.ReadLine();
+            
+        }
+
+        public void reverse()
+        {
+            String s2 = "";
+            for (int i=s1.Length-1;i>=0;i--)
+            {
+                s2=s2+s1[i]; 
+            }
+            Console.WriteLine(s2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            reverseString s1 = new();
+            s1.GetData();
+            s1.reverse();
+            Console.ReadKey();
+        }
+    }
+}
