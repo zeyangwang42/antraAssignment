221a7fd1e4d098ec4ac007fc34b099822c2974cd
zeyangwang42 <65516494+zeyangwang42@users.noreply.github.com>
2021-11-19 23:23:59 -0600

    Add files via upload

---
+++ c#assignment/Lab2/Exercise4/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Exercise4
+{
+    class Solution
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you want to enter a array as arr[A,B],Please enter your A size");
+            int A;
+
+            Int32.TryParse(Console.ReadLine(), out A);
+            Console.WriteLine("If you want to enter a array as arr[A,B],Please enter your B size");
+            int B;
+
+            Int32.TryParse(Console.ReadLine(), out B);
+            int[,]array = new int[A,B];
+            for (int i = 0; i < A; i++)
+            {
+                for (int j = 0; j < B; j++)
+                {
+                    Console.WriteLine("Please enter arr[ " + i +","+j + "] values");
+                    Int32.TryParse(Console.ReadLine(), out array[i,j]);
+                }
+                
+            }
+           
+            int up = 0;
+            int left = 0;
+            int right = B - 1;
+            int down = A - 1;
+            int Count = 0;
+            while (Count<A*B) {
+                
+                for (int col = left; col <= right; col++)
+                {
+                  
+                    Console.Write(array[up,col]+" ");
+                    Count++;
+                }
+                
+                for (int row = up + 1; row <= down; row++)
+                {
+                    Console.Write(array[row,right]+" ");
+                    Count++;
+                }
+                
+                if (up != down)
+                {
+                    
+                    for (int col = right - 1; col >= left; col--)
+                    {
+                        Console.Write(array[down,col]+" ");
+                        Count++;
+                    }
+                }
+                
+                if (left != right)
+                {
+                    
+                    for (int row = down - 1; row > up; row--)
+                    {
+                        Console.Write(array[row,left]+" ");
+                        Count++;
+                    }
+                }
+                left++;
+                right--;
+                up++;
+                down--;
+
+
+            }
+
+        }
+    }
+}
---
+++ c#assignment/Lab2/Exercise5/Program.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Exercise5
+{
+    class Box
+    {
+        double length, breadth, height;
+        public double getVolume()
+        {
+            return length * breadth * height;
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+        public void setBreadth(double bre)
+        {
+            breadth = bre;
+        }
+        public void setHeight(double hei)
+        {
+            height = hei;
+        }
+        public static Box operator +(Box b, Box c)
+        {
+            Box box = new Box();
+            box.length = b.length + c.length;
+            box.breadth = b.breadth + c.breadth;
+            box.height = b.height + c.height;
+            return box;
+        }
+        public static Box operator -(Box b, Box c)
+        {
+            Box box = new Box();
+            box.length = b.length - c.length;
+            box.breadth = b.breadth - c.breadth;
+            box.height = b.height - c.height;
+            return box;
+        }
+        
+        static void Main()
+        {
+            Box b1= new Box();
+            Box b2 = new Box();
+            Box b3 = new Box();
+
+            b1.setBreadth(2);
+            b1.setLength(4);
+            b1.setHeight(6);
+
+
+            b2.setBreadth(1);
+            b2.setLength(3);
+            b2.setHeight(4);
+
+            double volume;
+            volume = b1.getVolume();
+            Console.WriteLine("Volume if Box1： {0}", volume);
+            volume = b2.getVolume();
+            Console.WriteLine("Volume if Box1： {0}", volume);
+            b3 = b1 + b2;
+            volume = b3.getVolume();
+            Console.WriteLine("Volume if Box1： {0}", volume);
+        }
+    }
+}
